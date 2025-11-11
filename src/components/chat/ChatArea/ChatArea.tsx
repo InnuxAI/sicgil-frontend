c98ce9adb61d6e3d7220821bfa047be9a93311ee
Ubuntu <azureuser@linux-vm-001.zhyij1dv5eeejcio3x1uihzqce.cx.internal.cloudapp.net>
2025-11-11 07:32:40 +0000

    fix(chat): ensure chat input stays at bottom regardless of message count
    - Removed sticky positioning from ChatInput container
    - Wrapped MessageArea in flex-1 container with overflow-hidden
    - Added h-full to main container for proper height constraint
    - Chat input now naturally positions at bottom using flexbox layout
    - Fixes issue where input wasn't at bottom with few messages

--- src/components/chat/ChatArea/ChatArea.tsx
+++ src/components/chat/ChatArea/ChatArea.tsx
@@ -4,10 +4,11 @@ import ChatInput from './ChatInput'
 import MessageArea from './MessageArea'
 const ChatArea = () => {
   return (
-    <main className="relative m-1.5 flex flex-grow flex-col rounded-xl bg-background">
-      <MessageArea />
-      <div 
-        className="sticky bottom-0 ml-9 px-4 pb-2">
+    <main className="relative m-1.5 flex h-full flex-grow flex-col rounded-xl bg-background">
+      <div className="flex-1 overflow-hidden">
+        <MessageArea />
+      </div>
+      <div className="ml-9 px-4 pb-4">
         <ChatInput />
       </div>
     </main>
